45d6154dae012f1a394c91a97f3a23d9166e94cf
rinkurana4432 <dsingh0032@gmail.com>
2022-12-14 16:04:46 +0530

    Logical code Qusetions

--- logical.php
+++ logical.php
@@ -1,11 +1,11 @@
 <?php
 //function to convert string into array
-$arr = 'Sachin';
-$data = array();
-for($i=0;$i<strlen($arr);$i++){
-	$data[] = $arr[$i];
-}
-echo '<pre>';print_r($data);
+// $arr = 'Sachin';
+// $data = array();
+// for($i=0;$i<strlen($arr);$i++){
+	// $data[] = $arr[$i];
+// }
+// echo '<pre>';print_r($data);
 
 
 
@@ -44,14 +44,14 @@ output
 	4 5 6
 	7 8 9 10
 */   
-   $k = 1;
-	for($i = 1; $i<=4; $i++){
-		for($j=1; $j<=$i; $j++){
-			echo $k;
-			$k++;
-		}
-		echo "<br>";
-	}
+   // $k = 1;
+	// for($i = 1; $i<=4; $i++){
+		// for($j=1; $j<=$i; $j++){
+			// echo $k;
+			// $k++;
+		// }
+		// echo "<br>";
+	// }
    
    
    
@@ -70,8 +70,14 @@ output
        // echo "It is not a leap year";
      // }
 // }
-  
-  
+
+//Remove the duplicate value from an array.
+    // $number = array(1, 3, 4, 2, 1, 6, 4, 9, 7, 2, 9);
+	// $newArr = array();
+		// foreach($number as $val){
+			// $newArr[$val] = $val;
+		// }
+	// echo '<pre>'; print_r($newArr);
   
   
    ?>
@@ -94,3 +100,48 @@ output
   // }
   
 // }
+
+
+
+
+
+// $numbers=array(12,23,45,20,5,6,34,17,9,56);
+// $length=count($numbers);
+// $min=$numbers[0];
+// for($i=1;$i<$length;$i++)
+  // {
+      // if($numbers[$i]<$min)
+        // {
+          // $min=$numbers[$i];
+        // }
+  // }
+// echo "The smallest number is ".$min;
+/*
+
+?>
+//Draw a Chess Table
+<table width="270px"  border="1px">  
+  <?php 
+       for($row=1;$row<=8;$row++){
+		   echo '<tr>';
+		   
+		   for($col=1;$col<=8;$col++){
+			   $total=$row+$col;  
+			   if($total % 2 == 0){
+				   
+				  echo '<td style="width:25px;height:25px;background-color:#fff;"></td>';
+			   }else{
+				   echo '<td style="width:25px;height:25px;background-color:#000;"></td>';
+			   }
+			   
+		   }
+		echo '</tr>';
+   }
+  
+  
+  ?>
+
+
+</table>
+
+
