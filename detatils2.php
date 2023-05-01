5f25967b827e5d405574b026db61ac7f1963df00
Rinkurana4432 <88030603+Rinkurana4432@users.noreply.github.com>
2023-05-01 09:22:31 +0530

    Add files via upload

---
+++ detatils2.php
@@ -0,0 +1,160 @@
+{{ $title[$service_title] }}@if (!$loop->last),@endif
+
+
+https://embed-googlemap.com/
+
+
+
+office@creeksideorthomt.com
+
+//Serach In Foreach loop
+$search = $city;
+if($search !=""){
+	if(! preg_match("/{$search}/i", $hotelresult['@attributes']['destination'])) {
+	  continue;
+		}
+}
+
+@if(!$taskStatus->isEmpty())
+
+onKeyPress="if(this.value.length==10 && event.keyCode!=8) return false;"
+
+
+ <a href="{{ route('home') }}"> <img style="max-width:70px;" src="http://genie124.info/local/images/settings/1671711319.png"> </a>
+ 
+  DB::enableQueryLog();
+  
+  $query = DB::getQueryLog();
+dd($query);
+
+[product id="13581"]
+____________________________________________________________________
+Jquery Functions
+____________________________________________________________________
+-------------------------
+Show random images using class
+
+jQuery(document).ready(function(){
+
+var rand = Math.floor(Math.random() * 3);
+	console.log('rand='+rand);
+jQuery('.hero-main').hide();
+jQuery('.hero-'+rand).show();
+})
+
+
+_--------------------------
+
+
+
+
+function remove_dallar(val){
+			return val.replace("$", "");
+		}
+function remove_comma(val){
+			return val.replace(/,/g , '');
+		}
+function numberWithCommas(number) {
+			var parts = number.toString().split(".");
+			parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+			return parts.join(".");
+		}
+function validateEmail(sEmail) {
+			var filter = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+			if (filter.test(sEmail)) {
+				return true;
+				} else {
+				return false;
+			}
+		}
+input_int();
+		function input_int(){
+			$('body').find(".int_input" ).keypress(function (e) {
+				if (e.which != 8 && e.which != 0 && e.which != 46 && (e.which < 48 || e.which > 57)) {
+					return false;
+				}
+			});
+		}
+function find_duplicate_in_array(array) {
+			var result = [];
+			for(var i = 0; i <= array.length; i++) {
+				for(var j = i; j <= array.length; j++) {
+					if(i != j && array[i] == array[j]) {
+						if(array[i] != 0){
+							result.push(i);
+						}
+						if(array[j] != 0){
+							result.push(j);
+						}
+					}
+				}
+			}
+			return result;
+		}
+//Show div on change Drop Down*/
+
+$('#h_city').on('change', function () {
+		
+		 var chage_rating_oncity = $('#rating').val();
+		  var chagecity = $(this).val();
+		  
+		  
+		if(chage_rating_oncity == ''){
+			
+			 if(chagecity){
+				$(".hotel_list_row").not(".city_" + chagecity).hide();
+				$(".city_" + chagecity).show();
+				$('.clsbtns').hide();
+			 }
+		}else if(chage_rating_oncity != ''){
+			
+			    $(".hotel_list_row").not(".rating_" + chage_rating_oncity).hide();
+				$(".rating_" + chage_rating_oncity).show();
+				$('.clsbtns').hide();
+			
+			if(chagecity){
+				
+				$(".hotel_list_row").not(".city_" + chagecity).hide();
+				//$(".city_" + chagecity).show();
+				$('.clsbtns').hide();
+			 }
+			
+		}	 
+    });		
+		
+__________________________________________________________
+
+https://prnt.sc/h9u45szGcbei
+
+
+	
+			$stripe = new \Stripe\StripeClient([
+			  "api_key" => "sk_test_4eC39HqLyjWDarjtT1zdp7dc",
+			  // "api_key" => "pk_live_upLdMU0DAwYfTgOrinaY82hV00bdTvDdu8",
+			  "stripe_version" => "2022-11-15"
+			]);
+			
+			$subscriptions =  $stripe->subscriptions->search([
+			  'limit' => 10,
+			  //'query' => 'status:\'active\'',
+			  'query' => 'status:\'canceled\'',
+			]);
+			
+	var error=false;
+	 jQuery('.glsr-field').each(function(){
+		if(jQuery(this).hasClass('glsr-is-invalid') || jQuery(this).val() ==''){
+			error = true;
+		}
+	});	 
+if(error == false ){			
+	jQuery('.wp-block-button__link').click(function(){
+     var textareavl = jQuery('#glsr-review-form glsr-form').text();	
+	jQuery('#space').before('<textarea  id="copyforclip"  placeholder="LEAVE A COMMENT TO SHARE YOUR EXPERIENCE WITH OTHERS." rows="5" required="">"+ textareavl +"</textarea>');	
+	
+    jQuery('.glsr-review-form glsr-form').hide();	
+});	
+
+}		
+ glsr-form-message glsr-form-success
+			
+			
